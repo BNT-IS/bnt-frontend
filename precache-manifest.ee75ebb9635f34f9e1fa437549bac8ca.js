f37845c6b34e5203b6ab6814d898ad815a029f71
Fabian Busch <fabian.busch@onlinehome.de>
2020-05-22 12:53:58 +0200

    Updates

---
+++ precache-manifest.ee75ebb9635f34f9e1fa437549bac8ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a722fe2984dbf2d64a473374d1d4318c",
+    "url": "/bnt-frontend/index.html"
+  },
+  {
+    "revision": "e6c29e30a64be90753bf",
+    "url": "/bnt-frontend/static/css/main.69b147b9.chunk.css"
+  },
+  {
+    "revision": "4d18a724339db7fba039",
+    "url": "/bnt-frontend/static/js/2.506cbafe.chunk.js"
+  },
+  {
+    "revision": "77a4c498e5d68f70484b4e0f78331187",
+    "url": "/bnt-frontend/static/js/2.506cbafe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c29e30a64be90753bf",
+    "url": "/bnt-frontend/static/js/main.90831bf6.chunk.js"
+  },
+  {
+    "revision": "0e5402bf4c445b695f4e",
+    "url": "/bnt-frontend/static/js/runtime-main.6bdd408e.js"
+  }
+]);
