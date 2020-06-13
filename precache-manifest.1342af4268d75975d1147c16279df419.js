a6991ea32a040c97a82195935e2df5ad86ec9186
Fabian Busch <fabian.busch@onlinehome.de>
2020-06-13 22:32:08 +0200

    Updates

---
+++ precache-manifest.1342af4268d75975d1147c16279df419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e266aa1b2f81b6877eeaa05f281e19f",
+    "url": "/bnt-frontend/index.html"
+  },
+  {
+    "revision": "a907c5aed4181a495c2b",
+    "url": "/bnt-frontend/static/css/main.f1f0b741.chunk.css"
+  },
+  {
+    "revision": "bea3a07461357f59c3ae",
+    "url": "/bnt-frontend/static/js/2.b9d39852.chunk.js"
+  },
+  {
+    "revision": "77a4c498e5d68f70484b4e0f78331187",
+    "url": "/bnt-frontend/static/js/2.b9d39852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a907c5aed4181a495c2b",
+    "url": "/bnt-frontend/static/js/main.7e2e41df.chunk.js"
+  },
+  {
+    "revision": "0e5402bf4c445b695f4e",
+    "url": "/bnt-frontend/static/js/runtime-main.6bdd408e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bnt-frontend/precache-manifest.0351f9b3921f30bd839ee6fdc077584d.js"
+  "/bnt-frontend/precache-manifest.1342af4268d75975d1147c16279df419.js"
 );
 
 self.addEventListener('message', (event) => {
