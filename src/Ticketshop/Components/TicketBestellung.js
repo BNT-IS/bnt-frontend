c633d1ea1e65113d8af517702313b558763ce005
Nils Niemann <niemann.nils@gmx.de>
2020-06-13 11:03:39 +0200

    Navigation angepasst

--- src/Ticketshop/Components/TicketBestellung.js
+++ src/Ticketshop/Components/TicketBestellung.js
@@ -6,30 +6,41 @@ class TicketBestellung extends React.Component {
 
     constructor(props) {
         super(props);
-        this.AbsolventenTicketAdd = this.AbsolventenTicketAdd.bind(this);
-        this.GuestTicketAdd = this.GuestTicketAdd.bind(this);
+        this.WindowAbsolventTicket = this.WindowAbsolventTicket.bind(this);
+        this.WindowGuestTicket = this.WindowGuestTicket.bind(this);
+        this.WindowParkTicket = this.WindowParkTicket.bind(this);
+        this.ToOverview = this.ToOverview.bind(this);
         this.ToPayment = this.ToPayment.bind(this);
     
-
         this.state = { 
             guestcount: 0, 
             parkcount: 0, 
             step: 0 
-        };
-        
+        };        
     }
 
 
-    AbsolventenTicketAdd(){
+
+    WindowAbsolventTicket(){
+        this.setState({ step: 0})
+    }
+    
+
+
+    WindowGuestTicket(){
         //Wechselt die Ansicht zu den GästeTickets
         this.setState({ step: 1 })
     }
-    GuestTicketAdd(){
+    WindowParkTicket(){
         this.setState({ step: 2})
     }
-    ToPayment(){
+    ToOverview(){
         this.setState({ step: 3})
     }
+    ToPayment(){
+        this.setState({ step: 4})
+    }
+
 
     //Funktion für die Counter
     increment = (property) => {
@@ -56,7 +67,7 @@ class TicketBestellung extends React.Component {
                {this.state.step === 0 &&
                 <Box gap="small">
                     Klicke hier, um ein Absolvententicket zu kaufen.
-                    <Button label=" Ein Absolventen Ticket kaufen" onClick={this.AbsolventenTicketAdd} gap="small"></Button>
+                    <Button label=" Ein Absolventen Ticket kaufen" onClick={this.WindowGuestTicket} gap="small"></Button>
                    
                 </Box>
                 }
@@ -68,7 +79,8 @@ class TicketBestellung extends React.Component {
                     <Button onClick={() => this.decrement("guest")} className="guestcount" label="-"></Button>
                     <h2>{this.state.guestcount}</h2>
 
-                   <Button onClick={this.GuestTicketAdd} label="Weiter"></Button>
+                    <Button onClick={this.WindowAbsolventTicket} label="Zurück"></Button>
+                    <Button onClick={this.WindowParkTicket} label="Weiter"></Button>
                 </Box>
                 }
 
@@ -79,7 +91,8 @@ class TicketBestellung extends React.Component {
                     <Button onClick={() => this.increment("park")} className="parkcount" label="+"></Button>
                     <Button onClick={() => this.decrement("park")} className="parkcount" label="-"></Button>
                     <h2>{this.state.parkcount}</h2>
-                    <Button onClick={this.ToPayment} label="Zur Bezahlung"></Button>
+                    <Button onClick={this.WindowGuestTicket} label="Zurück"></Button>
+                    <Button onClick={this.ToOverview} label="Weiter"></Button>
                 </Box>
                 }
 
@@ -87,14 +100,29 @@ class TicketBestellung extends React.Component {
                 <Box gap="small">
                     <Text>Sie haben folgendes bestellt: <br/>
                     Absolvententicket 1 <br/> 
-                    Gästeticket {this.state.guestcount} Parkticket {this.state.parkcount}
+                    Gästeticket {this.state.guestcount} <br/>
+                    Parkticket {this.state.parkcount}
+                    </Text>
+                    <Button onClick={this.WindowParkTicket} label="Zurück"></Button>
+                    <Button onClick={this.ToPayment}  label="Zahlungspflichtig bestellen"></Button>
+                </Box>
+                }
+                {this.state.step === 4 &&
+                <Box gap="small">
+                    <Text>Sie haben folgende Tickets Zahlungspflichtig bestellt.  <br/>
                     Bitte überweisen Sie folgenden Betrag auf das Konto: XXXXYYYYZZZZ.<br/>
                     Geben Sie ihren Namen als Verwendungszweck an.<br/>
                     Nach Rechnungseingang erhalten Sie Ihre Tickets an ihr Wallet gesendet.<br/>
+                    </Text>   
+                    <Text>Sie haben folgendes bestellt: <br/>
+                    Absolvententicket 1 <br/> 
+                    Gästeticket {this.state.guestcount} <br/>
+                    Parkticket {this.state.parkcount}
                     </Text>
-
                 </Box>
                 }
+
+
             </Box>
         );
     }
