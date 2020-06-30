7568c81f9fe5452a3b5afcfb0c054d58273b5567
Fabian Busch <fabian.busch@onlinehome.de>
2020-06-30 12:26:03 +0200

    Implemented frequently system state checking

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import SystemSetup from './SystemSetup/SystemSetup';
 import Ticketshop from './Ticketshop/Ticketshop';
 import Entrance from './Entrance/Entrance';
 import EventManagement from './EventManagement/EventManagement';
-import { Grommet, grommet } from 'grommet';
+import { Grommet, grommet, Box, Heading } from 'grommet';
 import { Switch, Route, Link } from "react-router-dom";
 
 import config from './config';
@@ -16,14 +16,41 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { userContext: null };
+    this.state = { userContext: null, setupMode: false };
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
     this.init = this.init.bind(this);
   }
 
   componentDidMount() {
+
+    // Check system status once mounted
+    this.detectSystemState();
+
+    // Check system status frequently (currently every 60 seconds)
+    window.setInterval(() => {
+      this.detectSystemState();
+    }, 60000);
+
+    // For login stuff
     this.init();
+  }
+
+  async detectSystemState() {
+    try {
+      var response = await fetch(config.BACKEND_BASE_URI, {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache'
+      })
+      if (response.status === 200) {
+        this.setState({ setupMode: false });
+      } else {
+        this.setState({ setupMode: true });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   init() {
@@ -61,35 +88,53 @@ class App extends React.Component {
       <UserContext.Provider value={Object.assign(this.state.userContext ? this.state.userContext : {}, { logout: this.logout, login: this.login, reloadLocalStorage: this.init })}>
 
         <Grommet theme={grommet}>
-          <Switch>
-            <Route exact path="/">
-              <ul>
-                <li><Link to="/guest">Ticketshop</Link></li>
-                <li><Link to="/entrance">Einlass</Link></li>
-                <li><Link to="/eventmgmt">Event-Management</Link></li>
-                <li><Link to="/setup">System-Setup</Link></li>
-                <li><Link to="/login">Anmelden</Link></li>
-              </ul>
-            </Route>
-          </Switch>
-          <Switch>
-            <Route path="/guest">
-              <Ticketshop eigenschaft1="test"></Ticketshop>
-            </Route>
-            <Route path="/entrance">
-              <Entrance></Entrance>
-            </Route>
-            <Route path="/eventmgmt">
-              <EventManagement></EventManagement>
-            </Route>
-            <Route path="/setup">
-              <SystemSetup></SystemSetup>
-            </Route>
-            <Route path="/login">
-              <Login></Login>
-            </Route>
-          </Switch>
+          {!this.state.setupMode &&
+            <Box>
+              <Switch>
+                <Route path="/guest">
+                  <Ticketshop eigenschaft1="test"></Ticketshop>
+                </Route>
+                <Route path="/entrance">
+                  <Entrance></Entrance>
+                </Route>
+                <Route path="/eventmgmt">
+                  <EventManagement></EventManagement>
+                </Route>
+                <Route path="/login">
+                  <Login></Login>
+                </Route>
+                <Route path="/">
+                  <ul>
+                    <li><Link to="/guest">Ticketshop</Link></li>
+                    <li><Link to="/entrance">Einlass</Link></li>
+                    <li><Link to="/eventmgmt">Event-Management</Link></li>
+                    <li><Link to="/login">Anmelden</Link></li>
+                  </ul>
+                </Route>
+              </Switch>
+            </Box>
+          }
+          {this.state.setupMode &&
+            <Box>
+              <Switch>
+                <Route path="/setup">
+                  <SystemSetup></SystemSetup>
+                </Route>
+                <Route path="/">
+                  <Box pad="medium" alignContent="center" direction="column" align="center">
+                    <Heading>Wartungsarbeiten</Heading>
+                    <p>
+                      Sehr geehrte Besucher*innen,<br />
+                      das Bachelorsnight Ticketsystem befindet sich derzeit im Wartungsmodus.<br />
+                      Bitte versuchen Sie es später erneut.
+                    </p>
+                  </Box>
+                </Route>
+              </Switch>
+            </Box>
+          }
         </Grommet>
+
       </UserContext.Provider>
     );
   }
